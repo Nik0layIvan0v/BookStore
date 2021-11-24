a1305dfa1f4d681799482337ff7206273049f239
Nikolay Ivanov <niki_8905@abv.bg>
2021-11-24 17:30:03 +0200

    Add BookStoreDbContext in BookStore.Data. Add Models folder in BookStore to hold return api models. Add Infrastructure folder to hold extensions methods like apply migrations and other. Add Common project to hold global common data.

---
+++ BookStore.Data/BookStoreDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace BookStore.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Reflection;
+
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+               : base(options)
+        {
+        }
+
+        //DbSets....
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Path to database!");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //base.OnModelCreating(modelBuilder); -> verify if needed!
+        }
+    }
+}
